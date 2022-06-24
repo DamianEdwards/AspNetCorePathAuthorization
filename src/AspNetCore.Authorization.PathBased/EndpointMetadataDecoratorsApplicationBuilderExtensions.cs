79543576ab7a45efe09caef2e83196690fb32549
Damian Edwards <damian@damianedwards.com>
2022-06-24 15:42:17 -0700

    Throw custom exception if services aren't added

--- src/AspNetCore.Authorization.PathBased/EndpointMetadataDecoratorsApplicationBuilderExtensions.cs
+++ src/AspNetCore.Authorization.PathBased/EndpointMetadataDecoratorsApplicationBuilderExtensions.cs
@@ -10,6 +10,9 @@ public static class EndpointMetadataDecoratorsApplicationBuilderExtensions
     /// Adds a <see cref="RouteEndpoint"/> to the <see cref="IEndpointRouteBuilder"/> that adds the provided metadata items to
     /// any <see cref="RouteEndpoint"/> mapped to HTTP requests for the specified pattern.
     /// </summary>
+    /// <remarks>
+    /// Requires <see cref="EndpointMetadataDecoratorServiceCollectionExtensions.AddEndpointMetadataDecorators(IServiceCollection)"/> to have been called.
+    /// </remarks>
     /// <typeparam name="T">The specific <see cref="IEndpointRouteBuilder"/> type.</typeparam>
     /// <param name="endpoints">The <typeparamref name="T"/> builder.</param>
     /// <param name="pattern">The route pattern.</param>
@@ -18,7 +21,13 @@ public static class EndpointMetadataDecoratorsApplicationBuilderExtensions
     public static IEndpointConventionBuilder MapMetadata<T>(this T endpoints, string pattern, params object[] items)
         where T : IEndpointRouteBuilder
     {
-        var _ = endpoints.ServiceProvider.GetRequiredService<MetadataOnlyEndpointMetadata>();
+        var servicesAdded = endpoints.ServiceProvider.GetService<MetadataOnlyEndpointMetadata>() is not null;
+
+        if (!servicesAdded)
+        {
+            throw new InvalidOperationException("Services required for endpoint metadata decorators have not been registered. " +
+                                                "Make sure IServiceCollection.AddEndpointMetadataDecorators() was called during app startup.");
+        }
 
         return endpoints.Map(pattern, EndpointMetadataDecoratorMatcherPolicy.NoOpRequestDelegate)
             .WithMetadata(new MetadataOnlyEndpointMetadata())
